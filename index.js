936eaaf79d009a34711e3e7a2ecaddf835e11ce3
Houssam <111168411+Lil-Late-06xX@users.noreply.github.com>
2022-11-13 00:28:25 -0800

    adding new site

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+const togl = document.querySelector('#mobile-menu')
+const menyLinks = document.querySelector('.nav-menu')
+
+
+togl.addEventListener('click' , function(){
+   togl.classList.toggle('is-active')
+   menyLinks.classList.toggle('active')
+})
+
+
